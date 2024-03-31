d47bde7faef84a0bc1ad6775c536464c7f524e7b
mariamiamore <165545199+mariamiamore@users.noreply.github.com>
2024-03-31 15:27:05 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace uwu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string rusername = "Admin";
+            string rpassword = "Admin1234!";
+            int attempts = 0;
+            while (attempts < 3) 
+            {
+                Console.Write("Username: ");
+                string username = Console.ReadLine();
+                Console.Write("Password: ");
+                string password = Console.ReadLine();
+
+                if (rusername == username && rpassword == password)
+                { 
+                    Console.WriteLine("Login Successful!");
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect Credentials. Please try again.");
+                    attempts++;
+                }
+            }
+            Console.WriteLine("Maximum login attempts exceeded.");
+            
+            
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# number1
+ a program which asks a user for their username and passwords. The username should be "Admin", while password must be "Admin1234!"  and prompt as login successful otherwise prompt the user that the credentials are incorrect. The user will have 3 attempts maximum .
